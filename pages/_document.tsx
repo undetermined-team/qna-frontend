1e8a39f943b3bfcfce15bd4d06617cc8a3546010
Kwon Hyeok Jin <80798626+lexky82@users.noreply.github.com>
2022-03-12 15:22:51 +0900

    feat: SSR 스타일 세팅

---
+++ pages/_document.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import { ServerStyleSheets } from "@mui/styles";
+
+export default function _Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+Document.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet();
+  const materialSheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          sheet.collectStyles(materialSheets.collect(<App {...props} />)),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
+};
