2ea05affb90ef34da36a954f18f5b4236f061416
Kwon Hyeok Jin <80798626+lexky82@users.noreply.github.com>
2022-03-12 15:44:00 +0900

    Design: Defualt Global Style

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -1,7 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    letter-spacing: -1px;
+    font-size: 15px;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  li {
+    list-style : none;
+  }
 `;
 
 export default GlobalStyle;
