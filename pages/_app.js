b13da07c0f49e727b22522768e3fdb1fbb3536f0
Kwon Hyeok Jin <80798626+lexky82@users.noreply.github.com>
2022-03-19 23:54:53 +0900

    feat: Theme 세팅 및 MUI Theme 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,23 @@
-import { useEffect } from 'react'
 import GlobalStyle from "../styles/globalStyles";
 import Layout from '../components/Layout';
 import { ThemeProvider } from "styled-components";
-import { theme } from "../styles/theme";
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+import { muiTheme, theme } from "../styles/theme";
 import CssBaseline from "@mui/material/CssBaseline";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <GlobalStyle />
+    <>
       <ThemeProvider theme={theme}>
-        <Layout>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </Layout>
+        <MuiThemeProvider theme={muiTheme}>
+          <GlobalStyle />
+          <Layout>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </Layout>
+        </MuiThemeProvider>
       </ThemeProvider>
-    </div>
+    </>
   );
 }
 
