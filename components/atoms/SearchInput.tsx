dd6e443b71eaffb4e0c37839c282fd056d80e727
Kwon Hyeok Jin <80798626+lexky82@users.noreply.github.com>
2022-03-20 21:35:22 +0900

    fix: border, padding 커스텀 가능하도록 수정

--- components/atoms/SearchInput.tsx
+++ components/atoms/SearchInput.tsx
@@ -3,7 +3,6 @@ import InputBase, { InputBaseProps } from "@mui/material/InputBase";
 
 export const SearchInput = muiStyled(InputBase)<InputBaseProps>(({ theme }) => ({
   "& .MuiInputBase-input": {
-    borderRadius: 13,
     position: "relative",
     backgroundColor: theme.palette.Gray100,
     border: `1px solid ${theme.palette.Gray200}`,
@@ -12,7 +11,6 @@ export const SearchInput = muiStyled(InputBase)<InputBaseProps>(({ theme }) => (
     lineHeight: 28,
     color: theme.palette.Gray400,
     paddingRight: "45px !important",
-    padding: "11px 18px",
     fontFamily: "Roboto",
 
     "&:placeholder": {
