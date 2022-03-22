70b5ace96bffe10113b0b45e700420d54a25622b
Kwon Hyeok Jin <80798626+lexky82@users.noreply.github.com>
2022-03-22 23:22:51 +0900

    fix: height sx props으로 조정 가능하도록 수정 및 lineHeight 삭제

--- components/atoms/FilterButton.tsx
+++ components/atoms/FilterButton.tsx
@@ -2,18 +2,19 @@ import { styled as muiStyled } from "@mui/system";
 import { Button } from "@mui/material";
 
 export const FilterButton = muiStyled(Button)(({ theme }) => ({
-  height: 36,
   marginRight: 4,
   padding: "10px 17px",
   fontFamily: "Roboto",
   fontStyle: "normal",
   fontWeight: 400,
   fontSize: 14,
-  lineHeight: 16,
   textAlign: "center",
   borderRadius: 2,
 
   "&:hover": {
+    backgroundColor: "#fff",
+  },
+  "&:active": {
     backgroundColor: "#fff",
   },
 }));
