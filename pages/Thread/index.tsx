c26a20d63641ecfd5a25ee1820fe65d2ab3b92cd
Kwon Hyeok Jin <80798626+lexky82@users.noreply.github.com>
2022-03-27 21:46:11 +0900

    feat: 명성 순위 컴포넌트 예시

--- pages/Thread/index.tsx
+++ pages/Thread/index.tsx
@@ -1,14 +1,36 @@
 import React from "react";
 import FilterArea from "./FilterArea";
 import { Tag } from "../../components/atoms/Tag";
-import DeleteTag from "../../components/molecules/DeleteTag";
 import AsideContainer from "../../components/organisms/AsideContainer";
 import { Space } from "../../assets/SvgIcons";
 import ThreadContainer from "../../components/organisms/ThreadContainer";
+import UserSummary from "../../components/molecules/UserSummary";
 
 const Thread = () => {
   const onDelete = (e) => {};
   const onTextButton = (e) => {};
+  const exmapleBountyRank = [
+    {
+      userName: "jojorabbit21",
+      bounty: 8750,
+    },
+    {
+      userName: "naerkf0123",
+      bounty: 7417,
+    },
+    {
+      userName: "hardoop_man",
+      bounty: 7104,
+    },
+    {
+      userName: "mandalore808",
+      bounty: 6415,
+    },
+    {
+      userName: "gookbab5000",
+      bounty: 5535,
+    },
+  ];
 
   return (
     <div style={{ display: "inline-flex" }}>
@@ -22,6 +44,7 @@ const Thread = () => {
             fontWeight: 400,
             fontSize: 13,
             color: "#616161",
+            lineHeight: "15.23px",
           }}
         >
           57개의 검색 결과가 있습니다.
@@ -104,19 +127,19 @@ const Thread = () => {
 
       <div style={{ borderRight: "1px solid #D6D6D6", padding: "0px 16px" }}>
         <AsideContainer title="검색한 태그" subButtonText="수정하기" onTextClick={onTextButton}>
-          <Tag label="javascript" />
-          <Tag label="node.js" />
-          <Tag label="svelte" />
-          <Tag label="next.js" />
-          <Tag label="java" />
-          <Tag label="ruby" />
-          <Tag label="react" />
+          <Tag style={{ marginRight: 6, marginBottom: 4, fontWeight: 600 }} label="javascript" />
+          <Tag style={{ marginRight: 6, marginBottom: 4, fontWeight: 600 }} label="node.js" />
+          <Tag style={{ marginRight: 6, marginBottom: 4, fontWeight: 600 }} label="svelte" />
+          <Tag style={{ marginRight: 6, marginBottom: 4, fontWeight: 600 }} label="next.js" />
+          <Tag style={{ marginRight: 6, marginBottom: 4, fontWeight: 600 }} label="java" />
+          <Tag style={{ marginRight: 6, marginBottom: 4, fontWeight: 600 }} label="ruby" />
+          <Tag style={{ marginRight: 6, marginBottom: 4, fontWeight: 600 }} label="react" />
         </AsideContainer>
 
         {Space}
 
         <AsideContainer title="최근에 본 질문" subButtonText="전체보기" onTextClick={onTextButton}>
-          <DeleteTag
+          {/* <DeleteTag
             style={{ display: "block" }}
             onDelete={onDelete}
             label="위도/경도 불러오는 함수 질문드립니다."
@@ -135,16 +158,22 @@ const Thread = () => {
             style={{ display: "block" }}
             onDelete={onDelete}
             label="Excel csv 저장시 맨 앞에 숫자 0을 붙..."
-          />
+          /> */}
         </AsideContainer>
 
         {Space}
 
-        <AsideContainer
-          title="명성 순위"
-          subButtonText="모두 보기"
-          onTextClick={onTextButton}
-        ></AsideContainer>
+        <AsideContainer title="명성 순위" subButtonText="모두 보기" onTextClick={onTextButton}>
+          {exmapleBountyRank.map((profile, i) => (
+            <UserSummary
+              userName={profile.userName}
+              bounty={profile.bounty}
+              index={i + 1}
+              key={i}
+              style={{ marginBottom: 14 }}
+            />
+          ))}
+        </AsideContainer>
       </div>
     </div>
   );
