6c2b4837e6243b9ead1865578740990613311579
kwon Hyeok Jin <lexky@naver.com>
2022-03-28 11:13:20 +0900

    fix: CSSOM을 생성하지 못하여 Document에서 line를 받아와 Font 적용하도록 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,12 @@ import { ServerStyleSheets } from "@mui/styles";
 export default function _Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
