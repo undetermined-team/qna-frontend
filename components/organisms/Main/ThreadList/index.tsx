d87fe54125d81f8fc9606dfc05cc7c7744d65112
kwon Hyeok Jin <lexky@naver.com>
2022-04-08 11:03:30 +0900

    Rename: MainPage에서만 사용하는 organisms 컴포넌트들 폴더 분리 및 경로 이동

--- components/organisms/Main/ThreadList/index.tsx
+++ components/organisms/Main/ThreadList/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import ThreadContainer, { ThreadContainerProps } from "../../molecules/ThreadContainer";
+import ThreadContainer, { ThreadContainerProps } from "../../../molecules/ThreadContainer";
 
 interface ThreadListProps {
   threadList: Array<ThreadContainerProps>;
