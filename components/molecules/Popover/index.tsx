c3ec3f9a3f27e15320f9e1e40eadb1b8b1d1d5ff
Kwon Hyeok Jin <80798626+lexky82@users.noreply.github.com>
2022-04-10 21:20:54 +0900

    feat: children을 클릭하면 렌더링 되는 Popover Molecules

---
+++ components/molecules/Popover/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { ArrowUp, PopoverWrapper } from "./styles";
+
+interface PopoverProps {
+  children: React.ReactNode;
+  contents: React.ReactNode;
+}
+
+const index: React.FC<PopoverProps> = (props) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <PopoverWrapper>
+      {isVisible && (
+        <>
+          <ArrowUp />
+          {props.contents}
+        </>
+      )}
+      <span onClick={() => setIsVisible(!isVisible)}>{props.children}</span>
+    </PopoverWrapper>
+  );
+};
+
+export default index;
---
+++ components/molecules/Popover/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const PopoverWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+`;
+
+export const ArrowUp = styled.div`
+  position: absolute;
+  top: 30px;
+  left: 20px;
+  width: 0;
+  height: 0;
+  border-left: 5px solid white;
+  border-right: 5px solid white;
+  border-bottom: 10px solid wheat;
+`;
