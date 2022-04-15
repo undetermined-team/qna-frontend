472273e8c379a5acb4a71a5d8763837677ced1d1
kwon Hyeok Jin <lexky@naver.com>
2022-04-15 13:39:40 +0900

    Design: ThreadButton 디자인 일부 수정 및 가상 선택자 추가

--- components/atoms/ThreadButton.tsx
+++ components/atoms/ThreadButton.tsx
@@ -1,10 +1,31 @@
 import styled from "styled-components";
 
 export const ThreadButton = styled.button`
-  margin-right: 5px;
+  display: flex;
+  align-items: flex-start;
+  ${(props) => props.theme.typography.Footnote2};
   background-color: #ffffff;
   border: 1px solid #e0e0e0;
   color: #757575;
+  height: 22px;
   padding: 4px 10px;
   cursor: pointer;
+
+  span {
+    margin-left: 4px;
+  }
+
+  &:hover {
+    border-color: ${(props) => props.theme.palette.Gray600};
+
+    svg {
+      path {
+        fill: ${(props) => props.theme.palette.Gray600};
+      }
+    }
+  }
+
+  &:active {
+    border-color: ${(props) => props.theme.palette.Gray900};
+  }
 `;
