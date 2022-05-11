30c9c9ba0c7d3cf44e8af62bd190026d81bd6159
kwon Hyeok Jin <lexky@naver.com>
2022-05-11 10:42:48 +0900

    feat: Signup 페이지 퍼블리싱 및 Formik 세팅

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,51 +1,229 @@
 import React from "react";
+import styled from "styled-components";
+import Button from "../components/atoms/Button";
 import { Input } from "../components/atoms/Input";
+import Link from "next/link";
+import { GithubIcon } from "../public/assets/SvgIcons";
+import { useFormik } from "formik";
 
-const signup = () => {
+const SignUpInput = styled(Input)`
+  ${(props) => props.theme.typography.Body};
+  width: 310px;
+  height: 38px;
+  padding: 10px;
+  border-radius: 4px;
+
+  & :hover {
+    border: 1px solid ${(props) => props.theme.palette.GreenA400};
+  }
+
+  & :active {
+    border: 1px solid ${(props) => props.theme.palette.GreenA400};
+    box-shadow: 0px 0px 0px 3px rgba(0, 230, 118, 0.25);
+  }
+
+  &:focus {
+    border: 1px solid ${(props) => props.theme.palette.GreenA400};
+  }
+`;
+
+const FormLabel = styled.p`
+  ${(props) => props.theme.typography.Footnote};
+  color: #000000;
+`;
+
+const SignUpButton = styled(Button)`
+  ${(props) => props.theme.typography.Body};
+  line-height: normal;
+  width: 310px;
+  height: 38px;
+  background-color: ${(props) => props.theme.palette.LightBlueA700};
+  border: ${(props) => props.theme.palette.LightBlueA700};
+  border-radius: 2px;
+  margin-bottom: 16px;
+`;
+
+const GithubSignUpButton = styled(Button)`
+  ${(props) => props.theme.typography.Body};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 310px;
+  height: 38px;
+  background-color: ${(props) => props.theme.palette.Gray900};
+  border-color: ${(props) => props.theme.palette.Gray900};
+  border-radius: 2px;
+  margin: 16px 0;
+
+  svg {
+    margin-right: 19px;
+  }
+`;
+
+const ConsentContents = styled.span`
+  ${(props) => props.theme.typography.Footnote};
+  color: ${(props) => props.theme.palette.Gray800};
+`;
+
+const ConsentCheckBox = styled.input`
+  width: 18px;
+  height: 18px;
+  border: 1px solid #bdbdbd;
+  margin-right: 10px;
+  border-radius: 4px;
+`;
+
+const ConsentWrapper = styled.div`
+  margin-bottom: 4px;
+  display: flex;
+  align-items: center;
+`;
+
+const SignUpContainer = styled.div`
+  margin-top: 113px;
+`;
+
+const SignUpHeading = styled.h2`
+  ${(props) => props.theme.typography.Heading1};
+  margin-bottom: 41px;
+  text-align: center;
+`;
+
+const FormInputWrapper = styled.div`
+  margin-bottom: 24px;
+`;
+
+const SignUpButtonContainer = styled.div`
+  text-align: center;
+`;
+
+const ValidityGuidelines = styled.p`
+  ${(props) => props.theme.typography.Body3};
+  color: ${(props) => props.theme.palette.Gray600};
+  width: 305px;
+  margin-top: 7px;
+`;
+
+const GoSignIn = styled.a`
+  ${(props) => props.theme.typography.Body};
+  color: ${(props) => props.theme.palette.LightBlueA700};
+  cursor: pointer;
+  margin-top: 33px;
+`;
+
+const Signup = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      usingAgreements: false,
+      marketingAgreements: false,
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const onGithubClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div style={{ display: "flex", marginTop: 96, flexDirection: "column" }}>
-      <h2 style={{ marginBottom: 61 }}>회원가입</h2>
+    <SignUpContainer>
+      <SignUpHeading>계정 만들기</SignUpHeading>
 
-      <form>
-        <div style={{ marginBottom: 20 }}>
-          <p style={{ fontSize: 13, marginBottom: 11 }}>이름</p>
-          <Input
+      <form onSubmit={formik.handleSubmit}>
+        <FormInputWrapper>
+          <FormLabel>이름</FormLabel>
+          <SignUpInput
+            required
             placeholder="김엘리자베스"
-            style={{ width: 310, height: 38, padding: 10, borderRadius: 4 }}
+            id="name"
+            name="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
           />
-        </div>
+        </FormInputWrapper>
 
-        <div style={{ marginBottom: 20 }}>
-          <p style={{ fontSize: 13, marginBottom: 11 }}>이메일</p>
-          <Input
+        <FormInputWrapper>
+          <FormLabel>이메일</FormLabel>
+          <SignUpInput
             type="email"
             placeholder="name@address.com"
-            style={{ width: 310, height: 38, padding: 10, borderRadius: 4 }}
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
           />
-        </div>
+        </FormInputWrapper>
 
-        <div style={{ marginBottom: 20 }}>
-          <p style={{ fontSize: 13, marginBottom: 11 }}>비밀번호</p>
-          <Input
+        <FormInputWrapper>
+          <FormLabel>비밀번호</FormLabel>
+          <SignUpInput
             type="password"
-            style={{ width: 310, height: 38, padding: 10, borderRadius: 4, marginBottom: 7 }}
+            required
+            name="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
           />
-          <p style={{ fontSize: 11, fontWeight: 400, color: "#757575", width: 305 }}>
+          <ValidityGuidelines>
             6-20자 이내의 대문자, 소문자, 숫자, 특수문자가 각 1개 이상 포함된 비밀번호를 만들어
             주세요.
-          </p>
-        </div>
+          </ValidityGuidelines>
+        </FormInputWrapper>
 
-        <div style={{ marginBottom: 20 }}>
-          <p style={{ fontSize: 13, marginBottom: 11 }}>비밀번호 확인</p>
-          <Input
+        <FormInputWrapper>
+          <FormLabel>비밀번호 확인</FormLabel>
+          <SignUpInput
             type="password"
-            style={{ width: 310, height: 38, padding: 10, borderRadius: 4, marginBottom: 7 }}
+            required
+            name="confirmPassword"
+            onChange={formik.handleChange}
+            value={formik.values.confirmPassword}
           />
-        </div>
+        </FormInputWrapper>
+
+        <FormInputWrapper>
+          <ConsentWrapper>
+            <ConsentCheckBox
+              type="checkbox"
+              required
+              name="usingAgreements"
+              onChange={formik.handleChange}
+              checked={formik.values.usingAgreements}
+            />
+            <ConsentContents>이용약관 및 개인정보 처리방침에 동의합니다.</ConsentContents>
+          </ConsentWrapper>
+
+          <ConsentWrapper>
+            <ConsentCheckBox
+              type="checkbox"
+              name="marketingAgreements"
+              onChange={formik.handleChange}
+              checked={formik.values.marketingAgreements}
+            />
+            <ConsentContents>마케팅 활용 동의 및 광고 수신에 동의합니다. (선택)</ConsentContents>
+          </ConsentWrapper>
+        </FormInputWrapper>
+
+        <SignUpButtonContainer>
+          <SignUpButton type="submit">가입하기</SignUpButton>
+
+          <p>또는</p>
+
+          <GithubSignUpButton onClick={onGithubClick}>
+            {GithubIcon}
+            <span>GitHub 계정으로 가입하기</span>
+          </GithubSignUpButton>
+
+          <Link href="/login">
+            <GoSignIn>이미 계정이 있으신가요?</GoSignIn>
+          </Link>
+        </SignUpButtonContainer>
       </form>
-    </div>
+    </SignUpContainer>
   );
 };
 
-export default signup;
+export default Signup;
