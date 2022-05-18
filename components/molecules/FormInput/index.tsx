ace540d14ce181a0d538b8c7b2cae7377a0e1b65
kwon Hyeok Jin <lexky@naver.com>
2022-05-18 11:05:54 +0900

    feat: FormInput Molecules

---
+++ components/molecules/FormInput/index.tsx
@@ -0,0 +1,73 @@
+import { FormikProps, FormikValues } from "formik";
+import React, { useState } from "react";
+import { HideEye, ShowEye } from "../../../public/assets/SvgIcons";
+import {
+  FormInputWrapper,
+  FormLabel,
+  PasswordInputWrapper,
+  PasswordShowButton,
+  SignInput,
+  ValidityGuidelines,
+} from "./styles";
+
+interface FormInputProps {
+  label?: string;
+  name: string;
+  placeholder?: string;
+  formik: FormikProps<FormikValues>;
+  required?: boolean;
+  type?: React.HTMLInputTypeAttribute;
+  variant?: "password" | undefined;
+}
+
+const index: React.FC<FormInputProps> = (props) => {
+  const [isPasswordShow, setIsPasswordShow] = useState(false);
+
+  const handleShowPassword: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    setIsPasswordShow(!isPasswordShow);
+  };
+
+  return props.variant === "password" ? (
+    <FormInputWrapper>
+      <FormLabel>{props.label}</FormLabel>
+      <PasswordInputWrapper>
+        <SignInput
+          style={{ paddingRight: 52 }}
+          isError={props.formik.getFieldMeta(props.name).error}
+          type={isPasswordShow ? "text" : "password"}
+          required
+          name={props.name}
+          {...props.formik.getFieldProps(props.name)}
+        />
+
+        <PasswordShowButton
+          style={{ position: "absolute", right: 12, top: 7 }}
+          onClick={handleShowPassword}
+        >
+          {isPasswordShow ? HideEye : ShowEye}
+        </PasswordShowButton>
+      </PasswordInputWrapper>
+
+      <ValidityGuidelines isError={props.formik.getFieldMeta(props.name).error}>
+        6-20자 이내의 대문자, 소문자, 숫자, 특수문자가 각 1개 이상 포함된 비밀번호를 만들어 주세요.
+      </ValidityGuidelines>
+    </FormInputWrapper>
+  ) : (
+    <FormInputWrapper>
+      <FormLabel>{props.label}</FormLabel>
+      <SignInput
+        isError={props.formik.getFieldMeta(props.name).error}
+        required={props.required}
+        placeholder={props.placeholder}
+        name={props.name}
+        type={props.type}
+        {...props.formik.getFieldProps(props.name)}
+      />
+
+      <ValidityGuidelines isError>{props.formik.getFieldMeta(props.name).error}</ValidityGuidelines>
+    </FormInputWrapper>
+  );
+};
+
+export default index;
